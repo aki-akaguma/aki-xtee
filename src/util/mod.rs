471f57fed4425effa29d39abd8cea0dbdecaac3e
aki <aki.akaguma@hotmail.com>
2021-04-24 08:29:07 +0900

    version 0.1.9

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,10 @@
-pub use self::adapt_output::{open_files, NameWrite};
-pub mod adapt_output;
-pub mod compress;
 pub mod err;
+
+mod opt_uc_x_param;
+pub use self::opt_uc_x_param::OptUcXParam;
+pub use self::opt_uc_x_param::OptUcXParamParseError;
+
+pub mod adapt_output;
+pub use self::adapt_output::{open_files, NameWrite};
+
+pub mod compress;
--- xtask/src/gen_src_cmd.rs
+++ xtask/src/gen_src_cmd.rs
@@ -9,6 +9,8 @@ pub fn do_gen_src() -> anyhow::Result<()> {
         |opt_str| {
             let tup = match opt_str.lon_or_sho() {
                 "tftp-blksize" => (false, false, MetaType::U32),
+                //
+                "X" => (false, true, MetaType::Other("opt_uc_x_param".into())),
                 _ => return None,
             };
             Some(FixupType::from_tuple(tup))
